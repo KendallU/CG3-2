6caa2f3aa31dafc1751e64f6eafcb4b7d6217b11
KendallU <kendallunion@gmail.com>
2018-07-29 15:06:10 -0500

    Add project files.

---
+++ Win Lose/CG3-2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Win_Lose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number: 1-10");
+            string userValue = Console.ReadLine();
+
+            if (userValue == "8")
+            {
+                string message = "You Win!";
+                Console.WriteLine(message);
+            }
+            else 
+            {
+                string message = "You Lose!";
+                Console.WriteLine(message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
